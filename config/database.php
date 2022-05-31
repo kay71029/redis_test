582d74aeaefa31d3cf2d9a42e144aad8b7604e67
kay <kay71029@gmail.com>
2022-05-31 06:02:08 +0000

    23

--- config/database.php
+++ config/database.php
@@ -108,12 +108,24 @@ return [
 
         'client' => 'predis',
 
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', 'localhost'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                ],
+            ],
         ],
+
+
+        // 'default' => [
+        //     'host' => env('REDIS_HOST', '127.0.0.1'),
+        //     'password' => env('REDIS_PASSWORD', null),
+        //     'port' => env('REDIS_PORT', 6379),
+        //     'database' => 0,
+        // ],
 
     ],
 
