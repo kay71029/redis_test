46af44cd56fc34aa6b102957a3168d2f29a3d3d2
kay <kay71029@gmail.com>
2022-05-31 06:18:29 +0000

    redis

--- config/database.php
+++ config/database.php
@@ -104,29 +104,45 @@ return [
     |
     */
 
+    // 'redis' => [
+
+    //     'client' => 'predis',
+
+    //     'clusters' => [
+    //         'default' => [
+    //             [
+    //                 'host' => env('REDIS_HOST', 'localhost'),
+    //                 'password' => env('REDIS_PASSWORD', null),
+    //                 'port' => env('REDIS_PORT', 6379),
+    //                 'database' => 0,
+    //             ],
+    //         ],
+    //     ],
+
+
+    //     // 'default' => [
+    //     //     'host' => env('REDIS_HOST', '127.0.0.1'),
+    //     //     'password' => env('REDIS_PASSWORD', null),
+    //     //     'port' => env('REDIS_PORT', 6379),
+    //     //     'database' => 0,
+    //     // ],
+
+    // ],
+
     'redis' => [
-
-        'client' => 'predis',
-
-        'clusters' => [
-            'default' => [
-                [
-                    'host' => env('REDIS_HOST', 'localhost'),
-                    'password' => env('REDIS_PASSWORD', null),
-                    'port' => env('REDIS_PORT', 6379),
-                    'database' => 0,
-                ],
-            ],
+        'cluster' => true,
+    
+        'default' => [
+            'host' => env('REDIS_HOST', 'localhost'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
         ],
-
-
-        // 'default' => [
-        //     'host' => env('REDIS_HOST', '127.0.0.1'),
-        //     'password' => env('REDIS_PASSWORD', null),
-        //     'port' => env('REDIS_PORT', 6379),
-        //     'database' => 0,
-        // ],
-
+    
+        'options' => [
+            'cluster' => 'redis',
+            'parameters' => ['password' => env('REDIS_PASSWORD', null)],
+        ],
     ],
 
 ];
